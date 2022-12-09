417dbc22f8fadde1d5abd36bdc827b4ba283b3b6
Hui Lan <lanhui@zjnu.edu.cn>
2022-12-09 13:19:36 +0800

    highlight.js: fix Bug 522.

--- app/static/js/highlight.js
+++ app/static/js/highlight.js
@@ -31,7 +31,17 @@ function highLight() {
         list[i] = list[i].replace(/(^\s*)|(\s*$)/g, ""); //消除单词两边的空字符
         if (list[i] !== "" && "<mark>".indexOf(list[i]) === -1 && "</mark>".indexOf(list[i]) === -1) {
 	    //将文章中所有出现该单词word的地方改为：" <mark>" + word + "<mark> "。 正则表达式RegExp()中，"\\s"代表单词前后必须要有空格，以防止只对单词中的部分字符高亮的情况出现。
-            articleContent = articleContent.replace(new RegExp("\\s"+list[i]+"\\s", "g"), " <mark>" + list[i] + "</mark> ");
+            let articleContent_fb = articleContent;  //文章副本
+            while(articleContent_fb.toLowerCase().indexOf(list[i]) !== -1){
+                //针对同一篇文章中可能存在相同单词的不同大小写问题，采用while循环判断副本中是否还存在匹配单词。
+
+                //找到副本中和list[i]匹配的第一个单词(第一种匹配情况),并赋值给list[i]。
+                list[i] = articleContent_fb.substr(articleContent_fb.toLowerCase().indexOf(list[i]),list[i].length);
+
+                articleContent_fb = articleContent_fb.replace(new RegExp("\\s"+list[i]+"\\s","g"),""); //删除副本中和list[i]匹配的单词
+
+                articleContent = articleContent.replace(new RegExp("\\s"+list[i]+"\\s", "g"), " <mark>" + list[i] + "</mark> ");
+            }
         }
     }
     document.getElementById("article").innerHTML = articleContent;
