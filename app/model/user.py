3e35679a91515425ec77573dc9a710860081dd2a
Awoodwhale <woodwhale@qq.com>
2023-03-23 17:09:25 +0800

    refactor: refactor the model

---
+++ app/model/user.py
@@ -0,0 +1,19 @@
+from model import *
+from Login import md5
+
+def get_users():
+    with db_session:
+        return User.select().order_by(User.name)[:]
+
+
+def update_password_by_username(username, password="123456"):
+    with db_session:
+        user = User.select(name=username)
+        if user:
+            user.first().password = md5(username + password)
+
+def update_expiry_time_by_username(username, expiry_time="20230323"):
+    with db_session:
+        user = User.select(name=username)
+        if user:
+            user.first().expiry_date = expiry_time
